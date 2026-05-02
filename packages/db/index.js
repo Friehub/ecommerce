211b653d891d4980693d1414eee0289febaa72b0
Naija-geek <onlineweb.spport@gmail.com>
2026-05-02 02:10:09 +0100

    fix: resolve db entrypoint using dynamic proxy for production and dev

--- packages/db/index.js
+++ packages/db/index.js
@@ -1,28 +1,8 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Decimal = exports.Prisma = exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-const globalForPrisma = global;
-exports.prisma = globalForPrisma.prisma ||
-    new client_1.PrismaClient({
-        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-    });
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-var client_2 = require("@prisma/client");
-Object.defineProperty(exports, "Prisma", { enumerable: true, get: function () { return client_2.Prisma; } });
-exports.Decimal = client_1.Prisma.Decimal;
-__exportStar(require("@prisma/client"), exports);
+const fs = require('fs');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production' || fs.existsSync(path.join(__dirname, 'dist/index.js'))) {
+  module.exports = require('./dist/index.js');
+} else {
+  module.exports = require('./index.ts');
+}
